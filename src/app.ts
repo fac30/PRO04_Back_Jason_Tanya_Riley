29889fe2d3d04b44c8b07b9a4a8b1b877c37ec36
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-22 16:56:12 +0100

    install cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import itemRoutes from './routes/itemRoutes';
 import authRoutes from './routes/authRoutes';
 const signup = require("./controllers/sign-up.ts");
 const login = require("./controllers/log-in.ts");
+const cookieParser = require('cookie-parser');
 
 // Uncomment the lines below if you need to create new tables or seed the database
 // import './models/dbInit';
@@ -13,6 +14,9 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json()); 
+
+const cookies = cookieParser(process.env.COOKIE_SECRET);
+app.use(cookies);
 
 app.use('/', itemRoutes);
 app.use('/auth', authRoutes);
