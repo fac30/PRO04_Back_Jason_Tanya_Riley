3cccf6fd76413df1086054468d4a35ca9013712b
Jason Warren <contact.jwarren@gmail.com>
2024-10-23 16:20:15 +0100

    fix: :card_file_box: uncomment session table

--- src/models/sessionModel.ts
+++ src/models/sessionModel.ts
@@ -5,13 +5,13 @@ const crypto = require('crypto');
 
 
 const insert_session: Statement = db.prepare(`
-    INSERT INTO sessions (id, buyer_id, expires_at)
-    VALUES (
-      $id,
-      $buyer_id,
-      DATE('now', '+7 days')
-    )
-  `);  
+	INSERT INTO sessions (id, buyer_id, expires_at)
+	VALUES (
+		$id,
+		$buyer_id,
+		DATE('now', '+7 days')
+	)
+`);  
 
 export function createSession(buyer_id: number): string {
     const id: string = crypto.randomBytes(64).toString('hex');
@@ -20,28 +20,28 @@ export function createSession(buyer_id: number): string {
 }
 
 const select_session: Statement = db.prepare(`
-    SELECT id, buyer_id, expires_at
-    FROM sessions
-    WHERE id = ?
-  `);
+	SELECT id, buyer_id, expires_at
+	FROM sessions
+	WHERE id = ?
+`);
 
 export function getSession(sid: string): Promise<{ id: string; buyer_id: number; expires_at: string } | undefined> {
-    return new Promise((resolve, reject) => {
-      select_session.get(sid, (err: Error | null, session: { id: string; buyer_id: number; expires_at: string }) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(session);
-        }
-      });
-    });
-  }
+	return new Promise((resolve, reject) => {
+		select_session.get(sid, (err: Error | null, session: { id: string; buyer_id: number; expires_at: string }) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(session);
+			}
+		});
+	});
+}
 
 const delete_session: Statement = db.prepare(`
-    DELERE FROM sessions WHERE id = ?
-    `);
+	DELETE FROM sessions WHERE id = ?
+`);
 
 export function removeSession(sid: string): void {
-    delete_session.run(sid);
+	delete_session.run(sid);
 }
 
