0d8f589f1db27a9177c957c64881c1abf5232a2d
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-29 11:33:37 +0000

    add frontend URL to the CORS

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ const app: Application = express();
 const SQLiteStore = connectSqlite3(session);
 
 app.use(cors({
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json()); 
