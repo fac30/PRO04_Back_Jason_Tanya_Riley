2219bda6ae2ef1079e8b68e4d51d0c1c9aacf14c
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-30 12:33:50 +0000

    add env.var to the .env file

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app: Application = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.DEPLOY_URL,
   credentials: true,
 }));
 app.use(express.json()); 
