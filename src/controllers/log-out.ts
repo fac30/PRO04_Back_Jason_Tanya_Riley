81f7d710a3900450fe9e99680f074412eefc456e
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-31 14:14:45 +0000

    add true to security

--- src/controllers/log-out.ts
+++ src/controllers/log-out.ts
@@ -2,12 +2,14 @@ import { Request, Response } from 'express';
 import 'express-session';
 
 export const logOut = async (req: Request, res: Response): Promise<void> => {
+    console.log('Logout function called'); // Log function entry
     req.session.destroy((err) => {
         if (err) {
             console.error('Error logging out:', err);
             return res.status(500).json({ message: 'Logout failed' });
-        }     
-        res.clearCookie('connect.sid'); // Clears the session cookie from the client
+        }
+        console.log('Session destroyed successfully');
+        res.clearCookie('connect.sid');
         res.status(200).json({ message: 'Logout successful' });
     });
 };
